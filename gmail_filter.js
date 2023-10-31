8bafe6428c3b901abe1032461047b35943b8768e
siampathan005@gmail.com <siampathan005@gmail.com>
2023-11-01 00:26:14 +0600

    add multiple gmail check

--- gmail_filter.js
+++ gmail_filter.js
@@ -5,19 +5,64 @@ require("dotenv").config();
 const key = process.env.API_KEY;
 //deactive email -> kapiladah@gmail.com
 
+// let verifier = new Verifier(key);
+// verifier.verify("siampathan005@gmail.com", (err, data) => {
+//   if (err) throw err;
+
+//   if (
+//     data.formatCheck == "true" &&
+//     data.smtpCheck == "true" &&
+//     data.dnsCheck == "true" &&
+//     data.disposableCheck == "false"
+//   ) {
+//     console.log(data);
+
+//     console.log(data.emailAddress, "Active");
+//   } else console.log("This Email not Active");
+//'at_dB7Bc8Ay80WXXQv421p5QqjSjwXuG'
+//});
+
+//multiple times gmail address
+//
+//
+//
+//
+//
+//
+//
+//using array
+const list_1 = [
+  "siampathan005@gmail.com",
+  "arumugamvaladi@gmail.com",
+  "kapiladah@gmail.com",
+  "siam@gmail.com",
+  "mmahadihassan@gmail.com",
+];
+
 let verifier = new Verifier(key);
-verifier.verify("siampathan005@gmail.com", (err, data) => {
-  if (err) throw err;
 
-  if (
-    data.formatCheck == "true" &&
-    data.smtpCheck == "true" &&
-    data.dnsCheck == "true" &&
-    data.disposableCheck == "false"
-  ) {
-    console.log(data);
+function emailTest(gmailTest) {
+  gmailTest.forEach((email) => {
+    verifier.verify(email, (err, data) => {
+      if (err) {
+        console.error(`Error verifying ${email}: ${err}`);
+        return;
+      }
 
-    console.log(data.emailAddress, "Active");
-  } else console.log("This Email not Active");
-  //'at_dB7Bc8Ay80WXXQv421p5QqjSjwXuG'
-});
+      if (
+        data.formatCheck == "true" &&
+        data.smtpCheck == "true" &&
+        data.dnsCheck == "true" &&
+        data.disposableCheck == "false"
+      )
+        console.log(data.emailAddress, "Active");
+      else console.log(data.emailAddress, "Not Active");
+
+      // console.log(
+      //   `${email} - SMTP Check: ${data.smtpCheck} ${data.freeCheck} ${data.formatCheck}`
+      // );
+    });
+  });
+}
+
+emailTest(list_1);
