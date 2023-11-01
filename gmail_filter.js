b5feb779a9c6446ce0a83e691774dfb9470c7dac
siampathan005@gmail.com <siampathan005@gmail.com>
2023-11-01 23:37:55 +0600

    add reverse contact API for filtering LinkedIn profile associated gmail

--- gmail_filter.js
+++ gmail_filter.js
@@ -31,38 +31,81 @@ const key = process.env.API_KEY;
 //
 //
 //using array
-const list_1 = [
-  "siampathan005@gmail.com",
-  "arumugamvaladi@gmail.com",
-  "kapiladah@gmail.com",
-  "siam@gmail.com",
-  "mmahadihassan@gmail.com",
-];
+// const list_1 = [
+//   "siampathan005@gmail.com",
+//   "arumugamvaladi@gmail.com",
+//   "kapiladah@gmail.com",
+//   "siam@gmail.com",
+//   "mmahadihassan@gmail.com",
+// ];
 
-let verifier = new Verifier(key);
+// let verifier = new Verifier(key);
 
-function emailTest(gmailTest) {
-  gmailTest.forEach((email) => {
-    verifier.verify(email, (err, data) => {
-      if (err) {
-        console.error(`Error verifying ${email}: ${err}`);
-        return;
-      }
+// function emailTest(gmailTest) {
+//   gmailTest.forEach((email) => {
+//     verifier.verify(email, (err, data) => {
+//       if (err) {
+//         console.error(`Error verifying ${email}: ${err}`);
+//         return;
+//       }
 
-      if (
-        data.formatCheck == "true" &&
-        data.smtpCheck == "true" &&
-        data.dnsCheck == "true" &&
-        data.disposableCheck == "false"
-      )
-        console.log(data.emailAddress, "Active");
-      else console.log(data.emailAddress, "Not Active");
+//       if (
+//         data.formatCheck == "true" &&
+//         data.smtpCheck == "true" &&
+//         data.dnsCheck == "true" &&
+//         data.disposableCheck == "false"
+//       )
+//         console.log(data.emailAddress, "Active");
+//       else console.log(data.emailAddress, "Not Active");
 
-      // console.log(
-      //   `${email} - SMTP Check: ${data.smtpCheck} ${data.freeCheck} ${data.formatCheck}`
-      // );
-    });
-  });
-}
+// console.log(
+//   `${email} - SMTP Check: ${data.smtpCheck} ${data.freeCheck} ${data.formatCheck}`
+// );
+//     });
+//   });
+// }
 
-emailTest(list_1);
+// emailTest(list_1);
+
+//find LinkedIn Profile gmail associated
+//
+//
+//
+//
+//
+//filter this type gmail
+
+//const API_KEY = "sk_live_653d361280aaf705ed17243c_key_y0wuva9wy";
+//bse URL =  https://api.reversecontact.com
+//Lst Url = /enrichment?apikey=API_KEY&email=TARGET_EMAIL
+
+// const request = require("request");
+
+// const API_KEY = "sk_live_653d361280aaf705ed17243c_key_y0wuva9wy";
+// const gmailAddress = "siampathan005@gmail.com";
+
+// async function findLinkedInProfileURL(gmailAddress) {
+//   const options = {
+//     url: `https://api.reversecontact.com/enrichment?apikey=${API_KEY}&email=${gmailAddress}`,
+//     method: "GET",
+//   };
+
+//   const response = await request(options);
+
+//   return JSON.parse(response.body);
+// }
+
+// async function findLinkedInProfileURLData(gmailAddress) {
+//   const targetEmailData = await findLinkedInProfileURL(gmailAddress);
+
+//   if (targetEmailData.linkedinProfileUrl) {
+//     console.log(
+//       "The LinkedIn profile URL is:",
+//       targetEmailData.linkedinProfileUrl
+//     );
+//   } else {
+//     console.log("The LinkedIn profile URL could not be found.");
+//   }
+// }
+
+// findLinkedInProfileURLData(gmailAddress);
