473de25a7522f5d8f54e51b89f79a59558bf6f8a
siampathan005@gmail.com <siampathan005@gmail.com>
2023-11-02 07:34:15 +0600

    API key issue fixed

--- gmail_filter.js
+++ gmail_filter.js
@@ -4,23 +4,33 @@ require("dotenv").config();
 
 const key = process.env.API_KEY;
 //deactive email -> kapiladah@gmail.com
+//concorsomusic2@yahoo.it
+//hemapt1@yahoo.com
+//delgadosergio@ig.com.br
+//anatorresmz@hotmail.com
+//ef.chou@msa.hinet.net
+//tiwi1@mac.com
+//justkelly@comcast.net
 
-// let verifier = new Verifier(key);
-// verifier.verify("siampathan005@gmail.com", (err, data) => {
-//   if (err) throw err;
+let verifier = new Verifier(key);
+verifier.verify("justkelly@comcast.net", (err, data) => {
+  if (err) throw err;
 
-//   if (
-//     data.formatCheck == "true" &&
-//     data.smtpCheck == "true" &&
-//     data.dnsCheck == "true" &&
-//     data.disposableCheck == "false"
-//   ) {
-//     console.log(data);
+  if (
+    data.formatCheck == "true" &&
+    data.smtpCheck == "true" &&
+    data.dnsCheck == "true" &&
+    data.disposableCheck == "false"
+  ) {
+    //console.log(data);
 
-//     console.log(data.emailAddress, "Active");
-//   } else console.log("This Email not Active");
-//'at_dB7Bc8Ay80WXXQv421p5QqjSjwXuG'
-//});
+    console.log(data.emailAddress, "Active");
+  } else {
+    console.log(data);
+    console.log(data.emailAddress, "This Email not Active");
+  }
+  //'at_dB7Bc8Ay80WXXQv421p5QqjSjwXuG'
+});
 
 //multiple times gmail address
 //
@@ -75,14 +85,15 @@ const key = process.env.API_KEY;
 //
 //filter this type gmail
 
-//const API_KEY = "sk_live_653d361280aaf705ed17243c_key_y0wuva9wy";
 //bse URL =  https://api.reversecontact.com
 //Lst Url = /enrichment?apikey=API_KEY&email=TARGET_EMAIL
+//
 
 // const request = require("request");
 
-// const API_KEY = "sk_live_653d361280aaf705ed17243c_key_y0wuva9wy";
+//const API_KEY = process.env.REVERSE_API;
 // const gmailAddress = "siampathan005@gmail.com";
+//sk_live_653d361280aaf705ed17243c_key_y0wuva9wy
 
 // async function findLinkedInProfileURL(gmailAddress) {
 //   const options = {
@@ -111,31 +122,31 @@ const key = process.env.API_KEY;
 // findLinkedInProfileURLData(gmailAddress);
 
 //add simple object for test purpose
-const info = {
-  email: "siampathan005@gmail.com",
-  emailType: "personal",
-  person: {
-    publicIdentifier: "mahadi-hassan-siam-pathan-0235a124a",
-    linkedInIdentifier: "ACoAAD2qzNsBhtTxL597GT0RdnrKUaw5qhuxbyI",
-    firstName: "Mahadi Hassan",
-    lastName: "Siam Pathan",
-    headline: "Student at Institute of Science and Technology (IST)",
-    location: "Dhaka Bangladesh",
-    followerCount: 26,
-    connectionCount: 26,
-    skills: [
-      "Git",
-      "SASS",
-      "Firebase",
-      "Front-End Development",
-      "React.js",
-      "JavaScript",
-      "Cascading Style Sheets (CSS)",
-    ],
-    linkedInUrl: "https://linkedin.com/in/mahadi-hassan-siam-pathan-0235a124a",
-  },
-};
+// const info = {
+//   email: "siampathan005@gmail.com",
+//   emailType: "personal",
+//   person: {
+//     publicIdentifier: "mahadi-hassan-siam-pathan-0235a124a",
+//     linkedInIdentifier: "ACoAAD2qzNsBhtTxL597GT0RdnrKUaw5qhuxbyI",
+//     firstName: "Mahadi Hassan",
+//     lastName: "Siam Pathan",
+//     headline: "Student at Institute of Science and Technology (IST)",
+//     location: "Dhaka Bangladesh",
+//     followerCount: 26,
+//     connectionCount: 26,
+//     skills: [
+//       "Git",
+//       "SASS",
+//       "Firebase",
+//       "Front-End Development",
+//       "React.js",
+//       "JavaScript",
+//       "Cascading Style Sheets (CSS)",
+//     ],
+//     linkedInUrl: "https://linkedin.com/in/mahadi-hassan-siam-pathan-0235a124a",
+//   },
+// };
 
-let find = info.person.linkedInUrl;
+// let find = info.person.linkedInUrl;
 
-console.log(`linkedInUrl: ${find}`);
+// console.log(`linkedInUrl: ${find}`);
