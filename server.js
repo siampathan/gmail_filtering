33b5225f4b057030a4ef4a5e8bcbcc11fc696b6c
siampathan005@gmail.com <siampathan005@gmail.com>
2023-12-13 12:29:47 +0600

    fixed some server issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 //email-verifier
 const Verifier = require("email-verifier");
-const key = process.env.API_Key;
+const key = "at_dB7Bc8Ay80WXXQv421p5QqjSjwXuG";
 let verifier = new Verifier(key);
 
 const app = express();
@@ -48,26 +48,30 @@ app.get("/", (req, res) => {
 //   });
 //   //return verifiedEmails;
 // }
-function emailVerifiers(email) {
-  return new Promise((resolve, reject) => {
-    verifier.verify(email, (err, data) => {
-      console.log(data);
-      if (err) {
-        reject();
-      }
-      if (
-        data?.formatCheck == "true" &&
-        data?.smtpCheck == "true" &&
-        data?.dnsCheck == "true" &&
-        data?.disposableCheck == "false"
-      ) {
-        resolve(data.emailAddress);
-      } else {
-        resolve(null);
-      }
-    });
-  });
-}
+//for verify email checker
+
+// function emailVerifiers(email) {
+//   return new Promise((resolve, reject) => {
+//     verifier.verify(email, (err, data) => {
+//       console.log(data);
+//       if (err) {
+//         reject();
+//       }
+//       if (
+//         data?.formatCheck == "true" &&
+//         data?.smtpCheck == "true" &&
+//         data?.dnsCheck == "true" &&
+//         data?.disposableCheck == "false"
+//       ) {
+//         resolve(data.emailAddress);
+//       } else {
+//         resolve(null);
+//       }
+//     });
+//   });
+// }
+
+//for deactive email verifey
 
 function emailUnVerifiers(email) {
   return new Promise((resolve, reject) => {
@@ -89,7 +93,7 @@ function emailUnVerifiers(email) {
   });
 }
 
-async function emailTester(emailList, type) {
+async function emailTester(emailList) {
   try {
     const promises = emailList.map((email) => emailUnVerifiers(email));
     const results = await Promise.all(promises);
@@ -115,7 +119,7 @@ app.post("/upload", upload.single("excelFile"), async (req, res) => {
   // const verifiedEmails = emailTest(newEmails);
   // const list = email_list.push(verifiedEmails);
 
-  console.log(verifiedEmails);
+  //console.log(verifiedEmails);
   res.json(verifiedEmails);
 });
 
